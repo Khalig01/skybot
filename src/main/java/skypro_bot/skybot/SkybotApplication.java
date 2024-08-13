2439a60add91c96cc3cb3f3a545ebf11a3ac0221
Khal01 <aliyev_56@list.ru>
2024-08-13 14:29:26 +0300

    bot

---
+++ src/main/java/skypro_bot/skybot/SkybotApplication.java
@@ -0,0 +1,13 @@
+package skypro_bot.skybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkybotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkybotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro_bot/skybot/SkybotApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro_bot.skybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
